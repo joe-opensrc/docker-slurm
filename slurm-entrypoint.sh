e5169438d840c7b5688604c5edaa281b7dbf1122
Joe OpenSrc <joe-opensrc@protonmail.ch>
2023-06-13 12:34:38 +0200

    refactor; slurm-entrypoint.sh

--- slurm-entrypoint.sh
+++ slurm-entrypoint.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
 # env overrides command line; defaults to slurmctld
+hname="$(hostname -s)"
+
 slurmType="${SLURM_TYPE:-${1:-slurmctld}}"
+SLURM_CLUSTERNAME="${SLURM_CLUSTERNAME:-ipc}"
+SLURM_SLURMCTLDHOST="${SLURM_SLURMCTLDHOST:-${hname}}"
+
+# echo "slurmType: ${slurmType}"
+# echo "hname: ${hname}"
+# echo "SLURM_CLUSTERNAME: ${SLURM_CLUSTERNAME}"
+# echo "SLURM_SLURMCTLDHOST: ${SLURM_SLURMCTLDHOST}"
+
+# Set debug option; runs in foreground + generates logs
 echo "${slurmType^^}_OPTIONS=\"-D\"" >>"/etc/default/${slurmType}"
 
-/etc/init.d/munge start
-
+# gen config from template
 source ~/Pyvirts/jinja2/bin/activate
+echo -ne "ClusterName=${SLURM_CLUSTERNAME}\nSlurmctldHost=${SLURM_SLURMCTLDHOST}" | jinja2 --format env /etc/slurm-llnl/slurm.conf.j2 >/etc/slurm-llnl/slurm.conf
 
-hname="$(hostname -s)"
-echo -ne "ClusterName=${SLURM_CLUSTERNAME:-ipc}\nSlurmctldHost=${SLURM_SLURMCTLDHOST:-${hname}}" | jinja2 --format env /etc/slurm-llnl/slurm.conf.j2 >/etc/slurm-llnl/slurm.conf
+# start munge
+/etc/init.d/munge start &>/dev/null 
 
-/etc/init.d/${slurmType} start 
+# run command based on slurmType
+case "${slurmType}" in
+  slurmd|slurmctld) /etc/init.d/${slurmType} start;;
+  cli) ${@};;
+esac
+
